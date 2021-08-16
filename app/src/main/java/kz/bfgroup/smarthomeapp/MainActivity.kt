21a80d536e2b04610db3d06519c784b1c7617c6b
damirbaisalov <baisalovdamir@gmail.com>
2021-08-16 11:45:36 +0600

    MainActivity + MenuActivity

--- app/src/main/java/kz/bfgroup/smarthomeapp/MainActivity.kt
+++ app/src/main/java/kz/bfgroup/smarthomeapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package kz.bfgroup.smarthomeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton : Button = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            val intent = Intent(this,MenuActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
