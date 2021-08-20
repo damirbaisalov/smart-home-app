47ce6bcedc0f80f94fe3033f60e3c03116e7fdbd
damirbaisalov <baisalovdamir@gmail.com>
2021-08-20 17:24:41 +0600

    RegiUser but need BirthDateValidation

--- app/src/main/java/kz/bfgroup/smarthomeapp/registration/RegistrationActivity.kt
+++ app/src/main/java/kz/bfgroup/smarthomeapp/registration/RegistrationActivity.kt
@@ -3,10 +3,14 @@ package kz.bfgroup.smarthomeapp.registration
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.basgeekball.awesomevalidation.AwesomeValidation
+import com.basgeekball.awesomevalidation.ValidationStyle
+import com.basgeekball.awesomevalidation.utility.RegexTemplate
 import kz.bfgroup.smarthomeapp.R
 import kz.bfgroup.smarthomeapp.data.ApiRetrofit
 import okhttp3.ResponseBody
@@ -17,6 +21,10 @@ import java.util.*
 
 class RegistrationActivity : AppCompatActivity(), StreetListDialogFragment.OnInputNewListener {
 
+    private val bundle = Bundle()
+    private lateinit var registerButton: Button
+    private lateinit var validation: AwesomeValidation
+
     private lateinit var userFIOEditText: EditText
     private lateinit var userIINEditText: EditText
     private lateinit var userEmailEditText: EditText
@@ -26,20 +34,17 @@ class RegistrationActivity : AppCompatActivity(), StreetListDialogFragment.OnInp
     private lateinit var userEntranceEditText: EditText
     private lateinit var userTelephoneEditText: EditText
     private lateinit var userPasswordEditText: EditText
-    private lateinit var registerButton: Button
-    private lateinit var fields: Map<String, String>
-    private val bundle = Bundle()
-    private var okRequest = false
-    private lateinit var userPhoneFormatted: String
-    private lateinit var userNameSurnameFormatted: String
-    private lateinit var userDadNameFormatted: String
 
+    private var userNameSurnameFormatted: String = ""
+    private var userDadNameFormatted: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
         initViews()
+
+        formValidation()
 
         userAddressEditText.isFocusable = false
         userAddressEditText.setOnClickListener {
@@ -58,6 +63,12 @@ class RegistrationActivity : AppCompatActivity(), StreetListDialogFragment.OnInp
                 this,
                 DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     userBirthdayEditText.setText(""+ dayOfMonth + "." + (monthOfYear + 1) + "." + year)
+                    validation.addValidation(
+                        this,
+                        R.id.user_birthday,
+                        RegexTemplate.NOT_EMPTY,
+                        R.string.required_field
+                    )
                 },
                 year,
                 month,
@@ -68,28 +79,50 @@ class RegistrationActivity : AppCompatActivity(), StreetListDialogFragment.OnInp
         }
 
         registerButton.setOnClickListener {
-            userPhoneFormatted = userTelephoneEditText.text.toString()
-            userPhoneFormatted = userPhoneFormatted.substring(1,userPhoneFormatted.length)
 
-            val list = userFIOEditText.text.toString().split(" ")
-            userNameSurnameFormatted = list[0] + " " + list[1]
-            userDadNameFormatted = list[2]
+            if (validation.validate() &&
+                formatTelephoneNumber(userTelephoneEditText.text.toString()).isNotEmpty()
+                && userAddressEditText.text.toString().isNotEmpty()) {
 
-            bundle.putString("user_fullname", userNameSurnameFormatted)
-            bundle.putString("user_dadname", userDadNameFormatted)
-            bundle.putString("user_iin", userIINEditText.text.toString())
-            bundle.putString("user_email", userEmailEditText.text.toString())
-            bundle.putString("user_birthday", userBirthdayEditText.text.toString())
-            bundle.putString("user_apartment", userApartmentEditText.text.toString())
-            bundle.putString("user_entrance", userEntranceEditText.text.toString())
-            bundle.putString("user_telephone", userPhoneFormatted)
-            bundle.putString("user_password", userPasswordEditText.text.toString())
+                val list = userFIOEditText.text.toString().split(" ")
+                when(list.size) {
+                    1 -> {
+                        userNameSurnameFormatted = list[0]
+                        userDadNameFormatted = ""
+                    }
+                    2 -> {
+                        userNameSurnameFormatted = list[0] + " " + list[1]
+                        userDadNameFormatted = ""
+                    }
+                    3 -> {
+                        userNameSurnameFormatted = list[0] + " " + list[1]
+                        userDadNameFormatted = list[2]
+                    }
+                    else -> {
+                        userNameSurnameFormatted = list[0] + " " + list[1]
+                        userDadNameFormatted = list[2]
+                    }
+                }
 
-            sendSmsCode()
-            val dialog = CodeDialogFragment()
-            dialog.arguments = bundle
-            dialog.show(supportFragmentManager, "codeDialog")
+                bundle.putString("user_fullname", userNameSurnameFormatted)
+                bundle.putString("user_dadname", userDadNameFormatted)
+                bundle.putString("user_iin", userIINEditText.text.toString())
+                bundle.putString("user_email", userEmailEditText.text.toString())
+                bundle.putString("user_birthday", userBirthdayEditText.text.toString())
+                bundle.putString("user_apartment", userApartmentEditText.text.toString())
+                bundle.putString("user_entrance", userEntranceEditText.text.toString())
+                bundle.putString("user_telephone", formatTelephoneNumber(userTelephoneEditText.text.toString()))
+                bundle.putString("user_password", userPasswordEditText.text.toString())
 
+                sendSmsCode()
+
+            } else if (validation.validate() && userAddressEditText.text.toString().isEmpty()) {
+                showToast("Выберите адрес из списка")
+            } else if (validation.validate() && formatTelephoneNumber(userTelephoneEditText.text.toString()).isEmpty()) {
+                showToast("Введите номер")
+            } else {
+                showToast("Заполните все поля")
+            }
         }
     }
 
@@ -104,21 +137,83 @@ class RegistrationActivity : AppCompatActivity(), StreetListDialogFragment.OnInp
         userTelephoneEditText = findViewById(R.id.user_telephone)
         userPasswordEditText = findViewById(R.id.user_password)
         registerButton = findViewById(R.id.submit_user_data_button)
+
+        validation = AwesomeValidation(ValidationStyle.BASIC)
     }
 
     private fun sendSmsCode() {
-        ApiRetrofit.getApiClient().sendSMSCode(userPhoneFormatted).enqueue(object: Callback<ResponseBody>{
+        ApiRetrofit.getApiClient().sendSMSCode(formatTelephoneNumber(userTelephoneEditText.text.toString())).enqueue(object: Callback<ResponseBody>{
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
                 if (response.isSuccessful) {
-                    showToast(response.body()!!.string())
-//                    okRequest = response.body()!!.string() == "Ok"
+                    if (response.body()!!.string() == "Ok") {
+                        showToast(response.body()!!.string())
+                        val dialog = CodeDialogFragment()
+                        dialog.arguments = bundle
+                        dialog.show(supportFragmentManager, "codeDialog")
+                    } else {
+                        showToast(response.body()!!.string())
+                    }
                 }
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
 //                okRequest = false
+                showToast(t.message)
             }
         })
+    }
+
+    private fun formValidation() {
+
+        validation.addValidation(
+            this,
+            R.id.user_name_surname,
+            RegexTemplate.NOT_EMPTY,
+            R.string.required_field
+        )
+
+        validation.addValidation(
+            this,
+            R.id.user_iin,
+            RegexTemplate.NOT_EMPTY,
+            R.string.required_field
+        )
+
+        validation.addValidation(
+            this,
+            R.id.user_email,
+            Patterns.EMAIL_ADDRESS,
+            R.string.email_format_wrong
+        )
+
+        validation.addValidation(
+            this,
+            R.id.user_apartment,
+            RegexTemplate.NOT_EMPTY,
+            R.string.required_field
+        )
+
+        validation.addValidation(
+            this,
+            R.id.user_entrance,
+            RegexTemplate.NOT_EMPTY,
+            R.string.required_field
+        )
+
+        validation.addValidation(
+            this,
+            R.id.user_telephone,
+            RegexTemplate.NOT_EMPTY,
+            R.string.required_field
+        )
+
+        validation.addValidation(
+            this,
+            R.id.user_password,
+            ".{6,}",
+            R.string.required_field
+        )
+
     }
 
     override fun inputAddress(street: String?, number: String?) {
@@ -129,6 +224,15 @@ class RegistrationActivity : AppCompatActivity(), StreetListDialogFragment.OnInp
             bundle.putString("user_street", street)
             bundle.putString("user_street_nomer", number)
         }
+    }
+
+    private fun formatTelephoneNumber(tel: String): String {
+        var telephoneNumber = tel.replace("(","")
+        telephoneNumber = telephoneNumber.replace(")", "")
+        telephoneNumber = telephoneNumber.replace("-","")
+        telephoneNumber = telephoneNumber.replace("+","")
+
+        return telephoneNumber
     }
 
     private fun showToast(text: String?) {
